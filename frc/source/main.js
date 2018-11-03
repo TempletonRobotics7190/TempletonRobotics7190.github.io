94e4e8d554b9d9f70c09c8258bae6431f81f5c11
John Ivison <john@ivison.org>
2018-11-02 21:52:33 -0700

    Fixed sponsors resizing bug

--- frc/source/main.js
+++ frc/source/main.js
@@ -6,12 +6,16 @@ function codedisappear() {
     document.getElementById("code").innerHTML="&#60;&#62;"
 }
 
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip(); 
-});
-
 function smoothscroll(element_css_selector){
     document.querySelector(element_css_selector).scrollIntoView({ 
   behavior: 'smooth' 
 });
+}
+
+function sizeIn(id) {
+    document.getElementById(id).style.transform = ""
+}
+
+function sizeOut(id) {
+    document.getElementById(id).style.transform = "scale(0.95, 0.95)";
 }
